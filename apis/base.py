7641e04e75c6caf0a3403046f34e9d35cae808b0
NITHYA VIJAYAKUMAR <129824745+nithya2kp@users.noreply.github.com>
2023-11-11 15:55:14 +0530

    Delete apis directory

--- apis/base.py
+++
@@ -1,5 +0,0 @@
-from  fastapi import APIRouter
-from apis.v1 import  route_users
-
-api_router = APIRouter()   # Create an instance of APIRoute
-api_router.include_router(route_users.router,prefix="",tags=["users"])   # Include the user routes from the route_users
